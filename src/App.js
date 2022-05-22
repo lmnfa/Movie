b14ba1d18f469b56180cd5edd3618637b71b61c7
Sevinc <101855650+lmnfa@users.noreply.github.com>
2022-05-22 11:32:29 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+// import './index.html';
+import MovieList from './components/MovieList';
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([
+{
+    "Title": "Star Wars: Episode III - Revenge of the Sith",
+    "Year": "2005",
+    "imdbID": "tt0121766",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
+},
+{
+    "Title": "Star Wars: Episode II - Attack of the Clones",
+    "Year": "2002",
+    "imdbID": "tt0121765",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg"
+},
+{
+    "Title": "Rogue One: A Star Wars Story",
+    "Year": "2016",
+    "imdbID": "tt3748528",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg"
+},
+])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className='container-fluid movie-app'>
+      <div className='row'>
+      <MovieList movies={movies} />
+      </div>
     </div>
   );
 }
